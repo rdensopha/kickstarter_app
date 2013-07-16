f70e0e2a016e7229dba1b5c614913f86afb7b610
rdensopha <rdensopha@yahoo.com>
2013-07-16 11:36:11 +0530

    first commit of kickstarter app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KickStarterApp::Application.config.secret_token = '764f713f77bbf0a1282fdfdfab915893ed9b2003f1bfc7bce86785e7db944401da4f050d052a379faa02c56739b553be536742db716e072c54c35606fff40b25'
